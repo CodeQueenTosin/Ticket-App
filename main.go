95e1f66420738ef379517eecf16f90ed747d2397
Tosin <tosino@examroom.ai>
2023-10-04 13:07:51 +0100

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//Built in function to get, Containers to help
+//how to find the package to knownwhat to use
+
+func main() {
+
+	var conferenceName = "Go Conference"
+	const totalTickets = 50
+
+	var remainingTickets = 50
+
+	//fmt.Scan()
+	//fmt.Printf
+	//Arrays and Slices
+
+	var bookings = [50]string{"Tomi", Nik}
+
+	fmt.Println("Welcome to", conferenceName, "Booking Application")
+	fmt.Println("We have!", totalTickets)
+	fmt.Println("Get your tickets here!")
+	fmt.Println("We have", remainingTickets)
+
+}
